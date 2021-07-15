0b03a8c63dae8a0912465f5bd17075fd23c32c7a
Mike Kowalski <mike.kowalski@introhive.com>
2021-07-15 14:37:57 -0300

    try sh -c

--- do_stuff.sh
+++ do_stuff.sh
@@ -1,5 +1,14 @@
-
-echo "doing stuff"
+echo "Installing requirements"
 pip install -r requirements.txt
+echo "Running training"
 python train.py
-echo "FINISHED doing stuff"
+echo "FINISHED training"
+echo "creating report"
+echo "## Model Metrics" > report.md
+
+#    - cat metrics.txt >> report.md
+#    - echo "## Data Viz" >> report.md
+#    - cml-publish feature_importance.png --md >> report.md
+#    - cml-publish residuals.png --md >> report.md
+#    - cml-send-comment report.md
+

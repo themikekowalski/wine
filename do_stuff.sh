b3edeaf6fd050e17a283632ab623166ec339f75c
Mike Kowalski <mike.kowalski@introhive.com>
2021-07-15 16:32:51 -0300

    use env flag

--- do_stuff.sh
+++ do_stuff.sh
@@ -1,16 +1,14 @@
 echo "Installing requirements"
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
-#echo "Running training"
-#python train.py
-env
-env | grep repo_token
+echo "Running training"
+python train.py
 
-#echo "Creating report"
-#echo "## Model Metrics" >> report.md
-#cat metrics.txt >> report.md
-#echo "## Data Viz" >> report.md
-#cml-publish feature_importance.png --md >> report.md
-#cml-publish residuals.png --md >> report.md
-#cml-send-comment report.md
+echo "Creating report"
+echo "## Model Metrics" >> report.md
+cat metrics.txt >> report.md
+echo "## Data Viz" >> report.md
+cml-publish feature_importance.png --md >> report.md
+cml-publish residuals.png --md >> report.md
+cml-send-comment report.md
 
